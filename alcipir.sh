f000c152e0d804cfe436d5292081ccc28cc456f4
Andre Cabrera <andrecabrera@protonmail.ch>
2019-06-03 13:10:33 -0300

    Add GNU Screen config

--- alcipir.sh
+++ alcipir.sh
@@ -3,4 +3,5 @@ ln -sfv $(pwd)/bash/.bashrc ~/.bashrc
 ln -sfv $(pwd)/bash/.bash_profile ~/.bash_profile
 ln -sfv $(pwd)/bash/.alcipir.bash ~/.alcipir.bash
 ln -sfv $(pwd)/vim/.vimrc ~/.vimrc
+ln -sfv $(pwd)/misc/.screenrc ~/.screenrc
 
