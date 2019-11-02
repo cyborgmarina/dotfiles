349433e2e667191f9c2fd18e6c2201ea3f58e3ac
Andre L Cabrera <andre@cabrera.pw>
2019-11-01 22:02:21 -0300

    Zsh config, onefetch and more

--- alcipir.sh
+++ alcipir.sh
@@ -3,4 +3,6 @@ ln -sfv $(pwd)/zsh/.zshrc ~/.zshrc
 ln -sfv $(pwd)/zsh/.alcipir.zsh ~/.alcipir.zsh
 ln -sfv $(pwd)/vim/.vimrc ~/.vimrc
 ln -sfv $(pwd)/misc/.screenrc ~/.screenrc
+ln -sfv $(pwd)/misc/.alacritty.yml ~/.alacritty.yml
+ln -sfv $(pwd)/.config/alacritty/ ~/.config/alacritty
 
