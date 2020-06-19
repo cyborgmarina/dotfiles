3c7c45bfbe416204b17c60c1263844748de06b40
Andre Cabrera <andrecabrera@protonmail.ch>
2020-06-19 12:18:40 -0300

    Update script for code extensions

---
+++ scripts/install_code_extensions.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension eg2.vscode-npm-script
+code --install-extension esbenp.prettier-vscode
+code --install-extension joelday.docthis
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension yzhang.markdown-all-in-one
