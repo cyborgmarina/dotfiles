4409879518599136778b5f9478e497a6b6a35ebc
Andre Cabrera <andrecabrera@protonmail.ch>
2020-07-01 06:01:11 -0300

    Remove vscode config, add xresources

--- alcipir.sh
+++ alcipir.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-ln -sfv $(pwd)/code/settings.json $HOME/.config/Code/User/settings.json
 ln -sfv $(pwd)/shell/.bashrc $HOME/.bashrc
+ln -sfv $(pwd)/shell/.Xresources $HOME/.Xresources
 mkdir -p $HOME/.shell
 ln -sfv $(pwd)/shell/.alias $HOME/.shell/.alias
 ln -sfv $(pwd)/shell/.env $HOME/.shell/.env
 ln -sfv $(pwd)/shell/.prompt $HOME/.shell/.prompt
 ln -sfv $(pwd)/git/.gitconfig $HOME/.gitconfig
-source $HOME/.bashrc
+exec bash
--- shell/.bashrc
+++ shell/.bashrc
@@ -9,3 +9,6 @@ fi
 source $HOME/.shell/.prompt
 source $HOME/.shell/.env
 source $HOME/.shell/.alias
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
