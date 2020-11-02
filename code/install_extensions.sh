deebc659ac7cb06d00b55693eb6f1ec4e5052b95
Andre Cabrera <andrecabrera@protonmail.ch>
2020-11-02 13:06:40 -0300

    vscode: add install extensions script

---
+++ code/install_extensions.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension esbenp.prettier-vscode
+code --install-extension gaearon.subliminal
+code --install-extension llvm-vs-code-extensions.vscode-clangd
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-python.python
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
