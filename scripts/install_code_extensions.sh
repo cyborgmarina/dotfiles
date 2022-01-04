b90af8626623cd4f2e6c32a866b221cd62ee7ebe
Andre Cabrera <alcipir@Andres-Mac-mini.local>
2022-01-04 18:30:15 -0300

    cleanup - removing unused code

--- scripts/install_code_extensions.sh
+++ scripts/install_code_extensions.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-code --install-extension dbaeumer.vscode-eslint
-code --install-extension dsznajder.es7-react-js-snippets
-code --install-extension eg2.vscode-npm-script
-code --install-extension esbenp.prettier-vscode
-code --install-extension joelday.docthis
-code --install-extension msjsdiag.debugger-for-chrome
-code --install-extension yzhang.markdown-all-in-one
+code --install-extension JuanBlanco.solidity
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-vscode.vscode-typescript-next
+code --install-extension rust-lang.rust
