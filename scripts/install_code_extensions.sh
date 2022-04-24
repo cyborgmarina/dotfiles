a15181ab03c062a403f42bef7a225c2fcf5cba26
André Cabrera <andre@andrecabrera.com>
2022-04-24 08:56:34 -0300

    remove unused/poorly understood config files

--- scripts/install_code_extensions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-code --install-extension JuanBlanco.solidity
-code --install-extension ms-python.python
-code --install-extension ms-python.vscode-pylance
-code --install-extension ms-toolsai.jupyter
-code --install-extension ms-vscode.vscode-typescript-next
-code --install-extension rust-lang.rust
