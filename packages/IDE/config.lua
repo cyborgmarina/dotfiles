8a09942ff0625c2954f254d6dde38995b59c3d1f
André Cabrera <andre@andrecabrera.com>
2022-07-05 16:27:27 -0300

    feat: fzf and coc for nvim

--- packages/IDE/config.lua
+++ packages/IDE/config.lua
@@ -1,7 +1,24 @@
+vim.g.mapleader = ','
+
+vim.api.nvim_set_keymap('n', '<c-P>',
+    "<cmd>lua require('fzf-lua').files()<CR>",
+    { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<c-B>',
+    "<cmd>lua require('fzf-lua').buffers()<CR>",
+    { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<c-G>',
+    "<cmd>lua require('fzf-lua').git_files()<CR>",
+    { noremap = true, silent = true })
+
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   use 'github/copilot.vim'
   use 'tpope/vim-fugitive'
   use 'tpope/vim-surround'
   use 'tpope/vim-commentary'
+  use { 'neoclide/coc.nvim', branch = 'release' }
+  use { 'junegunn/fzf', run = './install --bin', }
+  use { 'ibhagwan/fzf-lua', requires = { 'kyazdani42/nvim-web-devicons' } }
 end)
